2eead0f21f70e6ad2f5b4cb32dad674b1bca3248
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-25 13:19:37 +1300

    wow

--- src/classes/Project.ts
+++ src/classes/Project.ts
@@ -1,6 +1,59 @@
 import fetch from "cross-fetch";
 import { Session, UserAgent } from "../Consts";
 
+interface ProjectAPIResponse {
+  id: number,
+  title: string,
+  description: string,
+  instructions: string,
+  visibility: string,
+  public: boolean,
+  comments_allowed: boolean,
+  is_published: boolean,
+  author: {
+    id: number,
+    username: string,
+    scratchteam: boolean,
+    history: {
+      joined: string
+    },
+    profile: {
+      id: null | number, // unsure about this one
+      images: {
+        '90x90': string,
+        '60x60': string,
+        '55x55': string,
+        '50x50': string,
+        '32x32': string
+      }
+    }
+  }
+  image: string,
+  images: {
+    '282x218': string,
+    '216x163': string,
+    '200x200': string,
+    '144x108': string,
+    '135x102': string,
+    '100x80': string
+  },
+  history: {
+    created: string,
+    modified: string,
+    shared: string
+  },
+  stats: {
+    views: number,
+    loves: number,
+    favorites: number,
+    remixes: number
+  },
+  remix: {
+    parent: null | number,
+    root: null | number
+  }
+}
+
 class Project {
   creator: string;
   id: number;
@@ -10,11 +63,11 @@ class Project {
     this.id = id;
     this.session = session;
   }
-  
+
   /**
    * Gets the api.scratch.mit.edu response of the project
    */
-  async getAPIData() {
+  async getAPIData(): Promise<ProjectAPIResponse> {
     if (typeof this.scratchProjectAPI === "undefined") {
       console.log("wow")
       const apiFetch = await fetch(`https://api.scratch.mit.edu/projects/${this.id}`);
@@ -26,6 +79,12 @@ class Project {
     return this.scratchProjectAPI;
   }
 
+  /**
+   * Gets comments in the project
+   * @param offset The offset of comments
+   * @param limit The limit of comments to return
+   * @returns The API response
+   */
   async getComments(offset = 0, limit = 20) {
     const apiData = await this.getAPIData()
     const commentFetch = await fetch(`https://api.scratch.mit.edu/users/${apiData.author.username}/projects/${this.id}/comments?offset=${offset}&limit=${limit}`)

ad7eaf3789991b1d823512c5ddfae7a053aaf237
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-02-04 21:13:28 +1300

    add project testing

---
+++ tests/project.spec.js
@@ -0,0 +1,30 @@
+import { Tester } from "neotest";
+import { ScratchSession } from "../dist/index.js";
+
+const r = new Tester();
+const session = new ScratchSession();
+
+// basic variables
+const projectID = 614027907;
+const project = session.getProject(projectID);
+const apiData = await project.getAPIData();
+const comments = await project.getComments();
+
+r.test("make sure project stats are numbers", () => {
+  r.expectTypeOf(apiData.stats.views, "number");
+  r.expectTypeOf(apiData.stats.favorites, "number");
+  r.expectTypeOf(apiData.stats.loves, "number");
+  r.expectTypeOf(apiData.stats.remixes, "number");
+});
+
+r.test("make sure project comments are object", () => {
+  r.expectTypeOf(comments, "object");
+});
+
+r.test("make sure project comments are iterable", () => {
+  r.expectTypeOf(comments.forEach, "function");
+});
+
+r.test("make sure project creator is a string", () => {
+  r.expectTypeOf(project.creator, "string");
+});

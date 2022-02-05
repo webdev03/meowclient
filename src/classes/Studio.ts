c7a75a8cd5fb70109bdb4b9811bd11918605d8a4
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-02-06 11:48:47 +1300

    add setTitle and setDescription

--- src/classes/Studio.ts
+++ src/classes/Studio.ts
@@ -43,6 +43,42 @@ class Studio {
     }
     return this.scratchStudioAPI;
   }
+
+  /**
+   * Sets the title of the studio.
+   * @param value The value to set the title to
+   * @returns {number} The status of the request
+   */
+  async setTitle(value: string): Promise<number> {
+    const setFetch = await fetch(`https://scratch.mit.edu/site-api/galleries/all/${this.id}/`, {
+      method: "PUT",
+      body: JSON.stringify({
+        title: value
+      })
+    });
+    if (!setFetch.ok) {
+      throw new Error(`Could not set title - ${setFetch.statusText}`);
+    }
+    return setFetch.status;
+  }
+
+  /**
+   * Sets the description of the studio.
+   * @param value The value to set the description to
+   * @returns {number} The status of the request
+   */
+  async setDescription(value: string): Promise<number> {
+    const setFetch = await fetch(`https://scratch.mit.edu/site-api/galleries/all/${this.id}/`, {
+      method: "PUT",
+      body: JSON.stringify({
+        description: value
+      })
+    });
+    if (!setFetch.ok) {
+      throw new Error(`Could not set description - ${setFetch.statusText}`);
+    }
+    return setFetch.status;
+  }
 }
 
 export default Studio;
